a1962bb598443855877a3bb78fa9e72eb41fa955
Henri Chauvet <henri.chauvet@epitech.eu>
2023-02-17 17:49:12 +0100

    [ADD] Comments about the models

--- ApiRestPython/myApp/models.py
+++ ApiRestPython/myApp/models.py
@@ -4,6 +4,9 @@ from django.db import models
 # Category model
 # Here, we need to define all the fields we'll need for the Category model
 class Category(models.Model):
+    """
+    Category model with required fields
+    """
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
 
@@ -11,6 +14,9 @@ class Category(models.Model):
 # Product model
 # Here, we need to define all the fields we'll need for the Product model
 class Product(models.Model):
+    """
+    Product model with required fields
+    """
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     product_url = models.URLField()
